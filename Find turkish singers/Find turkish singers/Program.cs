187cdbd18051add984c83a3eb4773f7e292f302a
Omar Meriwani <omar.sirwan@gmail.com>
2017-06-14 23:09:37 +0300

    --

---
+++ Find turkish singers/Find turkish singers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace Find_turkish_singers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urlAddress = "https://tr.wikipedia.org/wiki/Serkan_Kaya";
+
+            System.Net.HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null)
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                string data = readStream.ReadToEnd();
+
+                response.Close();
+                readStream.Close();
+            }
+            //-----------------------------------------------------------------
+            //using (WebClient client = new WebClient())
+            //{
+            //    string htmlCode = client.DownloadString(urlAddress);
+            //}
+
+
+        }
+    }
+}
