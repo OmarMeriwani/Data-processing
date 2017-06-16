e9cd89684e46afec50ecd104fdd9cec04bc8264d
Omar Meriwani <omar.sirwan@gmail.com>
2017-06-16 20:35:38 +0300

    --

--- Find turkish singers/Find turkish singers/Program.cs
+++ Find turkish singers/Find turkish singers/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Net;
 using System.IO;
+using System.Data.SqlClient;
 
 namespace Find_turkish_singers
 {
@@ -19,38 +20,105 @@ namespace Find_turkish_singers
     }
     class Program
     {
+        static string connectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\Omar\Documents\GitHub\Data-processing\Find turkish singers\Find turkish singers\SingersDB.mdf;Integrated Security=True;User Instance=True";
         static void Main(string[] args)
         {
             string erkek = "https://tr.wikipedia.org/wiki/Kategori:T%C3%BCrk_erkek_%C5%9Fark%C4%B1c%C4%B1lar";
+            string erkek2 = "https://tr.wikipedia.org/w/index.php?title=Kategori:T%C3%BCrk_erkek_%C5%9Fark%C4%B1c%C4%B1lar&pagefrom=Mahmut+Tuncer#mw-pages";
             string kadin = "https://tr.wikipedia.org/wiki/Kategori:T%C3%BCrk_kad%C4%B1n_%C5%9Fark%C4%B1c%C4%B1lar";
-            string erkekdata = readUrl(erkek);
+            string kadin2 = "https://tr.wikipedia.org/w/index.php?title=Kategori:T%C3%BCrk_kad%C4%B1n_%C5%9Fark%C4%B1c%C4%B1lar&pagefrom=P%C4%B1nar+Aylin#mw-pages";
+            Console.WriteLine("Reading..");
+            string erkekdata = readUrl(kadin2);
+            Console.WriteLine("Parsing..");
             //Console.WriteLine(erkekdata);
-            getSingers(erkekdata);
+            SaveToDB(getSingers(erkekdata));
+            Console.WriteLine("List is finished..");
+            FindUnexistPagesInDB();
+            Console.WriteLine("Unavailable list finished..");
             Console.ReadLine();
         }
-        static string readUrl(string url)
-        {
-            System.Net.HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            string data = "";
-            if (response.StatusCode == HttpStatusCode.OK)
-            {
-                Stream receiveStream = response.GetResponseStream();
-                StreamReader readStream = null;
 
-                if (response.CharacterSet == null)
+        static void FindUnexistPagesInDB()
+        {
+            string notFound = "Vikipedi'de bu isimde bir madde bulunmamaktadır";
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlConnection conn2 = new SqlConnection(connectionString);
+            conn.Open();
+            conn2.Open();
+            SqlDataReader r = new SqlCommand("select Singer, link from Singers where availablePage is null",conn).ExecuteReader();
+            while (r.Read())
+            {
+                string page = readUrl(r[1].ToString());
+                string Exist = "";
+                if (page.Contains(notFound) == true || page.Contains("##Error") == true )
                 {
-                    readStream = new StreamReader(receiveStream);
+                    Exist = "'FALSE'";
                 }
                 else
                 {
-                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                    Exist = "'TRUE'";
                 }
+                new SqlCommand("Update singers set AvailablePage = "+ Exist +" where Singer = N'" + r[0].ToString() + "'", conn2).ExecuteNonQuery();
+            }
+            r.Close();
+            conn.Close();
+            conn2.Close();
+        }
+        static void SaveToDB(List<Singerr> list)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            foreach (Singerr s in list)
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("insert into singers (Singer,Link) values (N'" + s.Name + "', N'" + s.Link + "')");
+                    cmd.Connection = conn;
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    SqlCommand cmd = new SqlCommand("insert into Errors (Singer,Error) values (N'" + s.Name + "', N'" + ex.Message + "')");
+                    cmd.Connection = conn;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            conn.Dispose();
+            conn.Close();
+        }
+        static string readUrl(string url)
+        {
+            string data = "";
+            try
+            {
+                System.Net.HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Stream receiveStream = response.GetResponseStream();
+                    StreamReader readStream = null;
 
-                data = readStream.ReadToEnd();
+                    if (response.CharacterSet == null)
+                    {
+                        readStream = new StreamReader(receiveStream);
+                    }
+                    else
+                    {
+                        readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                    }
 
-                response.Close();
-                readStream.Close();
+                    data = readStream.ReadToEnd();
+
+                    response.Close();
+                    readStream.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("404"))
+                    return "##Error: 404";
+                else
+                    return "##Error: " + ex.Message;
             }
             return data;
         }
@@ -66,7 +134,7 @@ namespace Find_turkish_singers
                     break;
                 
                 int endOfFirstSinger = page.IndexOf('"', firstSinger) - 1;
-                string link =  page.Substring(firstSinger, endOfFirstSinger - firstSinger);
+                string link =  page.Substring(firstSinger, endOfFirstSinger - firstSinger + 1);
                 if (link.Contains(":") == true)
                 {
                     firstSinger = endOfFirstSinger;
