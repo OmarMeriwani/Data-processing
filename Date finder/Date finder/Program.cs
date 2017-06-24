dc726da10fe94e036689d6d4f852e4b7e17bd08c
Omar Meriwani <omar.sirwan@gmail.com>
2017-06-24 23:12:14 +0300

    --

--- Date finder/Date finder/Program.cs
+++ Date finder/Date finder/Program.cs
@@ -9,6 +9,8 @@ namespace Date_finder
     {
         static void Main(string[] args)
         {
+            //Even with constructor you cannot call the class
+            dataSample d ;
 
         }
         void MonthsFinder(string text)
@@ -16,4 +18,22 @@ namespace Date_finder
 
         }
     }
+    abstract class dataSample
+    {
+        string name;
+        int val;
+        //Creating constructor for an abstract class is a meaningless thing to do
+        public dataSample(int v, string namee)
+        {
+            name = namee;
+            val = v;
+        }
+        //But it's ok to create a methods, and these methods will be abstract as well
+        public int ValueSquare()
+        {
+            return val * val;
+        }
+    }
+
+
 }
---
+++ Date finder/Date finder/car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Date_finder
+{
+    enum FuelType
+    {
+        Diesel,
+        Hybrid,
+        Electric,
+        Petrol
+    }
+    abstract class car
+    {
+        Color car_color;
+        int number_of_doors;
+        float engine_size;
+        FuelType fuel_type;
+    }
+}
