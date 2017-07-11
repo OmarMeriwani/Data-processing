10edc3e82881ef43d53f7b88a3b740cdeffa436a
Omar Meriwani <omar.sirwan@gmail.com>
2017-07-11 23:53:09 +0300

    --

--- Date finder/Date finder/Program.cs
+++ Date finder/Date finder/Program.cs
@@ -9,29 +9,15 @@ namespace Date_finder
     {
         static void Main(string[] args)
         {
-            //Even with constructor you cannot call the class
-            dataSample d ;
 
         }
         void MonthsFinder(string text)
         {
+            string[] Emonths = { "january" , "february" , "march" , "april" , "may" , "june" , "july" , "august" , "september" , "october" , "november" , "december" };
+            string[] EmonthsSmall = { "jan" , "feb" , "mar" , "apr" , "may" , "jun" , "jul" , "aug" , "sep" , "sept" , "oct" , "nov" , "dec" };
+            string[] monthNum = { "1","2","3","4","5","6","7","8","9","10", "11","12" };
+            string[] monthNumWithZero = { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12" };
 
-        }
-    }
-    abstract class dataSample
-    {
-        string name;
-        int val;
-        //Creating constructor for an abstract class is a meaningless thing to do
-        public dataSample(int v, string namee)
-        {
-            name = namee;
-            val = v;
-        }
-        //But it's ok to create a methods, and these methods will be abstract as well
-        public int ValueSquare()
-        {
-            return val * val;
         }
     }
 
