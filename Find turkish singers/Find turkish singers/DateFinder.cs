97e46cc777b7ba604c1353d7b18ead7d18e60c9f
Omar Meriwani <omar.sirwan@gmail.com>
2017-08-16 00:28:25 +0300

    --

--- Find turkish singers/Find turkish singers/DateFinder.cs
+++ Find turkish singers/Find turkish singers/DateFinder.cs
@@ -7,6 +7,21 @@ namespace Find_turkish_singers
 {
     class DateFinder
     {
+        /*
+         * To find birthdate and death date you have to follow the following rules:
+         * - Word d. followed by space and then a 4 digits number.
+         * - word ö. followed by space and then a 4 digits number.
+         * 
+         * The year is: 4 digits number (between 1500 - current year).
+         * Full date is: day + month name +  year
+         * 
+         * To catch a date-year event:
+         * - year + yılında + cut the text until "," or "."
+         * - Full date + cut the text until "," or "."
+         * - in the description panel find a month name or year, then the word before them. ex: Doğum	27 Kasım 1955
+         * - cut the text until the character "-" followed by a year. ex: İkinci Bahar - 1998
+         * - in the first paragraph (Full date, city name, country name -  Full date, city name, country name)
+         */
         string[] TrMonths = {"ocak", "şubat", "mart", "nisan", "mayıs", "haziran", "temmuz","temmuz","ağustos","eylül","ekim","kasım","aralık" };
         
         class Eventt
@@ -20,7 +35,7 @@ namespace Find_turkish_singers
             int monthIndex = 0;
             while (monthIndex != -1)
             {
-                monthIndex = wholeText.IndexOfAny(TrMonths,monthIndex);
+                //monthIndex = wholeText.IndexOfAny(TrMonths,monthIndex);
             }
             return t;
         }
--- Find turkish singers/Find turkish singers/TurkPlaceFinder.cs
+++ Find turkish singers/Find turkish singers/TurkPlaceFinder.cs
@@ -7,18 +7,32 @@ namespace Find_turkish_singers
 {
     class TurkPlaceFinder
     {
-        //Before the end of the first string, before the paragraph end (.)
-        //Have the word "(il)" in the title.
-        //Have the word "İl merkezi" in the description part.
-        //Could have the word (şehir) in the first sentence.
-        //If a smaller unit were found, then the search should stop.
-        //Don't consider the "." that's preceded by a number as an end of the sentence.
-        //Search any link word in the first sentence to see if it is a place, if it was a larger place then set it as a parent. Search in the database first and then in the web.
-
+        /*
+          1- Before the end of the first string, before the paragraph end (.)
+          2- Have the word "(il)" in the title.
+          
+          City
+          1- Have the word "İl merkezi" in the description part.
+          2- Could have the word (şehir) in the first sentence.
+         
+          - If a smaller unit were found, then the search should stop.
+          - Don't consider the "." that's preceded by a number as an end of the sentence.
+          - Search any link word in the first sentence to see if it is a place, if it was a larger place then set it as a parent. Search in the database first and then in the web.
+        */
         //Get the words (latitude, longitude), and then the next two charachters until the character "<" for each place.
         string[] Province = { "il", "ildir", "ili", "ilinden" };
         string[] district = { "ilçe", "ilçesidir", "ilçesinden", "ilçedir", "ilçelerinden", "ilçesi" };
         string[] city = { "şehir", "şehridir", "şehri", "merkezidir", "kenttir", "şehirdir" };
         string[] language = { "Kürtçe" };
+        /*
+        - Word +  kökenli. in the first paragraph
+        - 
+        */
+        string[] origin = { "kökenli" };
+        /*
+         1- Have one of the words.
+         2- Is link
+        */
+        string[] musicType = {"Müzik", "Müziği"}
     }
 }
