ec699f31f2f6edb015b7ff0de0a2ed4bf09ad003
Omar Meriwani <omar.sirwan@gmail.com>
2017-09-06 23:20:38 +0300

    --

---
+++ BirthsMonthly.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].BirthsMonthly
+(
+    [Country] INT NOT NULL, 
+    [Year] INT NOT NULL, 
+    [Month] NVARCHAR(100) NOT NULL, 
+    [Births] INT NOT NULL, 
+    [id] INT NOT NULL  identity (1,1), 
+    [Datee] DATE NULL, 
+    [SexDate] DATE NULL, 
+    [SexDateAvgTemp] FLOAT NULL 
+)
---
+++ dbo.MaxMonths.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[MaxMonths] (
+    [Country] INT NOT NULL,
+    [Year]    INT            NOT NULL,
+    [Month]   NVARCHAR (100) NOT NULL,
+    [Births]  INT            NOT NULL,
+    [id]      INT            NOT NULL
+);
+
