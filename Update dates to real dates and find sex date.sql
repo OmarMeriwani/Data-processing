91a3498d586cf322c7abf72c19620e138b5710b5
Omar Meriwani <omar.sirwan@gmail.com>
2017-09-07 00:29:29 +0300

    --

---
+++ Update dates to real dates and find sex date.sql
@@ -0,0 +1,3 @@
+﻿update maxmonths set birthsdate = CAST(convert(char, Year) + '-' + convert (char , Month) +  '-01 ' as date     )
+
+update maxmonths set sexdate = DATEADD(day, -280, BirthsDate)
---
+++ dbo.Countries.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Countries] (
+    [Id]               INT            IDENTITY (1, 1) NOT NULL,
+    [title]            NVARCHAR (200) NOT NULL,
+    [WinterJul]        BIT            NULL,
+    [HemsphereDevided] BIT            NULL,
+    [WinterAvgMin]     FLOAT (53)     NULL,
+    [SummerAvgMax]     FLOAT (53)     NULL,
+    [Equator]          BIT            NULL,
+    [NorthPole] BIT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
