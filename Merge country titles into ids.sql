7c986cb1f80baee2fea54ef6475874d2afed1659
Omar Meriwani <omar.sirwan@gmail.com>
2017-09-06 23:08:07 +0300

    --

---
+++ Merge country titles into ids.sql
@@ -0,0 +1,11 @@
+﻿MERGE BirthsMonthly AS T
+USING Countries AS S
+ON (T.Country = S.title) 
+WHEN MATCHED 
+    THEN UPDATE SET T.Country = S.id;
+
+MERGE MaxMonths AS T
+USING Countries AS S
+ON (T.Country = S.title) 
+WHEN MATCHED 
+    THEN UPDATE SET T.Country = S.id;
