05944ee3dfe76706f336e7806f4c2b9b239eb058
Omar Meriwani <omar.sirwan@gmail.com>
2017-09-07 22:55:53 +0300

    --

--- Update dates to real dates and find sex date.sql
+++ Update dates to real dates and find sex date.sql
@@ -1,3 +1,3 @@
-﻿update maxmonths set birthsdate = CAST(convert(char, Year) + '-' + convert (char , Month) +  '-01 ' as date     )
+﻿update maxmonths set birthsdate = CAST(convert(char, Year) + '-' + convert (char , Month) +  '-' + '01 ' as date     )
 
 update maxmonths set sexdate = DATEADD(day, -280, BirthsDate)
